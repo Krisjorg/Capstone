e3602f5c1bcdd38d7b028d87acb7acbe6511c0fb
Kristen Jorgensen <kristen.jorgensen8@gmail.com>
2021-08-09 10:25:27 -0500

    Reformatting the app

--- Trivia App/code.js
+++ Trivia App/code.js
@@ -1,9 +1,15 @@
 let url = "https://jservice.io/api/clues"
 
+document.querySelector("#start").addEventListener("click", function(event) {
+    event.preventDefault()
+    document.querySelector(".rules-container").style.display = "none"
+    document.querySelector(".game-container").style.display = "block"
+})
 
 class gameCreation {
     constructor () {
         this.object = []
+        this.category = 0
     }
     //function to pick a random number between 1 and 100
     selectRandomNumber() {
@@ -11,7 +17,7 @@ class gameCreation {
         return randomNumber
     }
 
-    //function that creates the category based on the fetch response using a random number and appends that category to the page
+    //function that accesses and appends the category to the page
     createCategory(response) {
         
         let keyList = Object.values(response)
@@ -38,13 +44,16 @@ class gameCreation {
         questionDiv.append(chosenQuestion)
     }
 
+    //function selects a random section of a list of 100 for the category, question, and answer
     selectRandomObject(response) {
         let randomNumber = this.selectRandomNumber()
         let randomKey = response[randomNumber]
+        this.object = response
+        console.log(this.object)
         console.log(randomKey)
         this.createCategory(randomKey)
         this.createQuestion(randomKey)
-        
+        this.checkAnswer(randomKey)
     }
 
     //function to fetch from jService and get category/questions
@@ -52,9 +61,34 @@ class gameCreation {
         fetch(url)
         .then(response => response.json())
         .then(parsedResponse => this.selectRandomObject(parsedResponse))
+    }
 
+    checkAnswer(response) {
+        
+        let answer = response.answer.toLowerCase()
+        console.log(answer)
+        
+        let result = document.querySelector(".result")
+        let answerResponse = document.createElement("p")
+        let answerListener = document.querySelector("#submit")
+        let answerInput = document.querySelector(".answer")
+        console.group(answerListener)
+        answerListener.addEventListener("click", function () {
+            
+            if (answerInput.value === answer) {
+                answerResponse.innerHTML = "You got it right! On to the next question!"
+                result.append(answerResponse)
+            } else {
+                answerResponse.innerHTML = "That’s not right… Well, better hit the books and study up. Your score is reset. Try, try again!"
+                result.append(answerResponse)
+            }
+        })
     }
 }
 
 let playGame = new gameCreation()
 playGame.fetchRequest()
+
+// let answerListener = document.querySelector("#submit")
+// console.group(answerListener)
+// answerListener.addEventListener("click", playGame.checkAnswer())
